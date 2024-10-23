e6b65b3de88ae97bfd3974a509aef09d19d14a6a
lyndossantos <108936981+lyndossantos@users.noreply.github.com>
2024-10-23 08:44:05 -0300

    Delete SOCORROOO directory

--- SOCORROOO/graficos/informacoesGlobais.js
+++
@@ -1,8 +0,0 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json';
-
-async function buscarInformacoes(){
-    const res = await fetch(url);
-    const dados = res.json();
-    console.log (dados);
-}
-buscarInformacoes();
